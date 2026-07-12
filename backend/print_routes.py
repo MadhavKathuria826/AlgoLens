726cf55031a7b00cbd5d9b8dc433d2023e6240a1
Madhav Kathuria <madhavkathuria01@gmail.com>
2026-07-12 15:46:44 +0530

    Remove ignored files from tracking

--- backend/print_routes.py
+++
@@ -1,7 +0,0 @@
-from main import app
-
-print("Registered Routes:")
-for route in app.routes:
-    # Some routes (like Mount) might not have methods
-    methods = getattr(route, "methods", None)
-    print(f"Path: {route.path}, Methods: {methods}")
